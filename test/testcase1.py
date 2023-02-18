f549010aa264c3685847a5c028362be1b5df70ac
stethoma <stethoma@redhat.com>
2023-02-18 12:29:13 +0100

    update testcase1

--- test/testcase1.py
+++ test/testcase1.py
@@ -13,10 +13,18 @@ actual_title = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]
 expected_title = "PRODUCTS"
 
 if actual_title==expected_title:
-    print("Test Passed")
+    print("Login Test Passed")
+    driver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div').click()
+    description = driver.find_element(By.XPATH, '//*[@id="inventory_item_container"]/div/div/div[2]/div[2]').text
+    if description.__contains__("compromising style"):
+        driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+        if driver.find_element(By.XPATH, '//div[@id="shopping_cart_container"]/a/span').text == '1':
+            print("Test Passed for adding item to cart")
+        else:
+            print("Test failed to add item to cart")
 else:
-    print("Test Failed")
+    print("Login Test Failed")
+
+
 
 driver.close()
-
-
