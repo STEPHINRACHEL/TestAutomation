f1aa79c96a994c768c03c5b78373e4a74cdebc2c
stethoma <stethoma@redhat.com>
2023-03-23 22:35:04 +0100

    Add new test

--- saucedemo/browser.py
+++ saucedemo/browser.py
@@ -1,15 +1,18 @@
 from selenium.webdriver.common.by import By
 
-
-class Login:
-    def open(self, driver):
-        driver.get('https://www.saucedemo.com/')
-        driver.maximize_window()
+#
+# class Login:
+#     def open(self, driver):
+#         driver.get('https://www.saucedemo.com/')
+#         driver.maximize_window()
+#
 
 
 def login(driver, user):
-    open_url = Login()
-    open_url.open(driver)
+    # open_url = Login()
+    # open_url.open(driver)
+    driver.get('https://www.saucedemo.com/')
+    driver.maximize_window()
     driver.find_element(By.ID, "user-name").send_keys(f'{user}')
     driver.find_element(By.NAME, "password").send_keys('secret_sauce')
     driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
--- test/test3.py
+++ test/test3.py
@@ -6,9 +6,14 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Firefox()
 driver.get('https://www.saucedemo.com/')
 
-login(driver)
+login(driver, 'standard_user')
 
-
-
+driver.find_element(By.XPATH, '//div[@class="bm-burger-button"]/button').click()
+driver.find_element(By.XPATH, '//div[@class="bm-menu"]/nav/a[3]').click()
+if driver.find_element(By.XPATH, '//input[@id="login-button"]').is_displayed():
+    print("Logout successful.....!")
+else:
+    print("Logout failed......!")
 
 close(driver)
+
