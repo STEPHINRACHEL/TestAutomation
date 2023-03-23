b749a9c05eb8ac31e8041e34f65258c40b4a33c5
stethoma <stethoma@redhat.com>
2023-03-23 20:20:18 +0100

    Add class

--- saucedemo/browser.py
+++ saucedemo/browser.py
@@ -2,17 +2,25 @@ from selenium.webdriver.common.by import By
 
 
 class Login:
-    def __init__(self, driver):
-        self.driver = driver
+    def open(self, driver):
         driver.get('https://www.saucedemo.com/')
         driver.maximize_window()
 
 
 def login(driver, user):
-    Login(driver)
+    open_url = Login()
+    open_url.open(driver)
     driver.find_element(By.ID, "user-name").send_keys(f'{user}')
     driver.find_element(By.NAME, "password").send_keys('secret_sauce')
     driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
+
+
+def print_success(user):
+    print(f"Login Test with {user} Passed................. :)")
+
+
+def print_failure(user):
+    print(f"Login Test with {user} Failed...................:(")
 
 
 def close(driver):
--- test/test1.py
+++ test/test1.py
@@ -1,9 +1,12 @@
 
-from saucedemo.browser import login, close
+from saucedemo.browser import login, close, print_success, print_failure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
+
+user1 = 'standard_user'
+user2 = 'locked_out_user'
 
 # Test with standard_user
 login(driver, 'standard_user')
@@ -14,18 +17,18 @@ expected_title = "PRODUCTS"
 print(f"Expected title is {expected_title}")
 
 if actual_title == expected_title:
-    print("Login Test with standard_user Passed................. :)")
+    print_success(user1)
 
 else:
-    print("Login Test with standard_user Failed...................:(")
+   print_failure(user1)
 
 
 # Test with locked_out_user
 login(driver, 'locked_out_user')
 
 if driver.find_element(By.XPATH, '//*[@class="error-message-container error"]/h3').text.__contains__('locked out'):
-    print("Login Test with locked_out_user passed................:)")
+    print_success(user2)
 else:
-    print("Login Test with locked_out_user failed..................:(")
+    print_failure(user2)
 
 close(driver)
